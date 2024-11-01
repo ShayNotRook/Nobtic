dc5c3aba8eaeab4b16b9b101e5db9cf34963e322
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-11-01 15:31:30 +0330

    Added rest_framework app to settings.

--- core/settings.py
+++ core/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # Local Apps
     'scheduler',
     'users',
+    # 3rd Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
