9bfefe1762864074da67db84c74b4f3794604ccd
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-17 00:25:48 +0330

    Added a signal that automatically creates appointments when slot is created

---
+++ scheduler/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+from .models import AppointmentSlot
+
+@receiver(post_save, sender=AppointmentSlot)
+def generate_apps(sender, instance, created, **kwargs):
+    """
+    Signal to generate appointments when an AppointmentSlot is created.
+    """
+    if created:
+        service_duration = 60
+        instance.create_appointments(service_duration)
