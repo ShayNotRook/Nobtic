49184047b9552f3c1d824d3a34f921c717593214
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-22 20:27:51 +0330

    Added app_name variable to access reverse path

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from .views import all_apps
 
+app_name = 'scheduler'
+
 urlpatterns = [
     # API Views
     path('api/', include('scheduler.api.api_urls')),
