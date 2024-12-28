32b8d76e9e24da2df3b2a0edd494e923e767bff9
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-28 17:58:19 +0330

    Added some minor styling for future design

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+// import React from 'react'
+import "./dashboard.css"
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <>
+      {/* <h2 className="dashboard-header">Dashboard</h2> */}
+      <div className="dashboard">
+        <div className="appointments-box">
+          <h3 className="appointments-box-header">نوبت ها</h3>
+          <p>Number of appointments: </p>
+          <p>Number of upcoming appointments: </p>
+          <p>Number of past appointments: </p>
+        </div>
+        <div className="user-box">
+          <h3>User</h3>
+          <p>Username: </p>
+          <p>Email: </p>
+        </div>
+      </div>
+    </>
   )
 }
 
