f5e01af2641f192fb3978bca661117d4f5fcf974
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-30 18:46:28 +0330

    Added AppointmentViewset

--- scheduler/api/v1/api_views.py
+++ scheduler/api/v1/api_views.py
@@ -1,12 +1,13 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 from django.contrib.auth.decorators import login_required
 
-from scheduler.models import Salon, Service
+from scheduler.models import Salon, Service, AppointmentSlot, Appointment
 
-from .serializers import ServiceSerializer, SalonSerializer
+from .serializers import ServiceSerializer, SalonSerializer, AppSlotSerializer, AppointmentSerializer
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
@@ -23,3 +24,29 @@ class SalonRetrieve(RetrieveAPIView):
     
     def get_queryset(self):
         return Salon.objects.get(owner=self.request.user)
+    
+    
+
+class AppSlotViewSet(viewsets.ModelViewSet):
+    serializer_class = AppSlotSerializer
+    permissions = [IsAuthenticated]
+    
+    
+    def get_queryset(self):
+        return AppointmentSlot.objects.filter(salon__owner=self.request.user)
+    
+    # def retrieve(self, request, id: int, *args, **kwargs):
+    #     return AppointmentSlot.objects.filter(id=id, salon__owner=self.request.user)
+    
+    
+    
+class AppViewSet(viewsets.ModelViewSet):
+    serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self, **kwargs):
+        appointment_slot_id = self.kwargs.get('appointment_slot_id')
+        if appointment_slot_id:
+            return Appointment.objects.filter(slot__id=appointment_slot_id, slot__salon__owner=self.request.user)
+    
+        return Appointment.objects.none()
