523dfe07764cf0628bab088f6adb84ebdbf99afc
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-30 18:47:47 +0330

    Added appointment router.

--- scheduler/api/v1/urls.py
+++ scheduler/api/v1/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 
-from .api_views import ServiceViewSet
+from .api_views import ServiceViewSet, AppSlotViewSet, AppViewSet
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register(r'services', ServiceViewSet, basename='service')
+router.register(r'appointmentslots', AppSlotViewSet, basename='appointmentslot')
 
+appointments_router = DefaultRouter()
+appointments_router.register(r'appointments', AppViewSet, basename='appointment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('appointmentslots/<int:appointment_slot_id>/', include(appointments_router.urls))
 ]
