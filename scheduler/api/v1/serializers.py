83d6bd0ae7f3f66a40ad3ece62c561a986932c57
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-30 18:47:22 +0330

    Modified ServiceSerializer to show name field as read_only, and AppSerializer to serialize service field

--- scheduler/api/v1/serializers.py
+++ scheduler/api/v1/serializers.py
@@ -1,15 +1,38 @@
-from rest_framework.serializers import ModelSerializer
+# from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from scheduler.models import Service, Salon
+from scheduler.models import Service, Salon, AppointmentSlot, Appointment
 
-class ServiceSerializer(ModelSerializer):
+class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ['id', 'salon', 'name', 'duration_time']
-        read_only_fields = ['id', 'salon']
+        read_only_fields = ['id', 'salon', 'name']
         
         
-class SalonSerializer(ModelSerializer):
+class SalonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Salon
         fields = '__all__'
+        
+class AppointmentSerializer(serializers.ModelSerializer):
+    service = ServiceSerializer(read_only=True)
+    
+    class Meta:
+        model = Appointment
+        fields = ["id", "customer_name", "service", "app_start", "app_end", "taken", "slot"]
+
+     
+class AppSlotSerializer(serializers.ModelSerializer):
+    
+    appointments = AppointmentSerializer(many=True, read_only=True)
+    
+    def get_apps(self, obj):
+        return obj.all_appointments
+    
+    class Meta:
+        model = AppointmentSlot
+        fields = '__all__'
+        read_only_fields = ['id', 'salon']
+        
+
