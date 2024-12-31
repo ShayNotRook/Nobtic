69abc37902e7b7c5b962d5549b5d43788e5f0194
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-31 16:22:32 +0330

    Create views file for user detail

---
+++ users/api/api_views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics, permissions
+from .serializers import UserSerializer
+
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+    
+    
+    def get_object(self):
+        return self.request.user
