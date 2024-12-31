f897512aa2feb65d49329acde938464bcaeefef7
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2024-12-31 16:26:48 +0330

    Added user detail url to api urls

--- users/api/api_urls.py
+++ users/api/api_urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from .api_views import UserDetailView
+
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/detail/', UserDetailView.as_view(), name="user_detail"),
 ]
