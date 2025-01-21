12a0455aaa554505c94efc07bfa218def4d87769
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-01-22 00:15:25 +0330

    Added bot urls path, specifically apis

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('schedule/', include('scheduler.urls')),
     path('users/', include('users.urls')),
+    path('bot/api/', include('bot.urls'))
 ]
