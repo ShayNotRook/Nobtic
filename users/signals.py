004d6e4db76029f53cdb7f93e99b6fbf801a6cae
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-03 23:39:28 +0330

    Minor spacing

--- users/signals.py
+++ users/signals.py
@@ -10,7 +10,6 @@ def create_profile(sender, instance, created, **kwargs):
 
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
-    
 
 
 post_save.connect(create_profile, sender=User)
