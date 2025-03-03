9336f4cdc1ae1017705c131f9ad0d62a0d9e5600
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-03 23:31:57 +0330

    Added Logout components

---
+++ frontend/src/components/auth/Logout.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+
+const API: string = "http://localhost:8000/users/api/token";
+
+
+const Logout = () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    console.log(refreshToken)
+    const handleLogout = async(_: { preventDefault: () => void; }) => {
+        const response = await fetch(`${API}/blacklist/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json",
+            },
+            body: JSON.stringify({"refresh": refreshToken})
+        });
+        if (response.ok) {
+            localStorage.clear()
+            goToHomepage();
+        } else {
+            alert("Logout failed");
+        }
+    }
+
+    const navigate = useNavigate();
+    const handleRevert = () => {
+        navigate(-1);
+    }
+
+    const goToHomepage = () => {
+        navigate('/');
+    }
+    
+  return (
+    <div className='logout-actions'>
+        <h2>ایا میخواهید از پنل خود خارج شوید؟</h2>
+        <button className='logout-btn' onClick={handleLogout} >Logout</button>
+        <button className='cancel-btn' onClick={handleRevert}>Cancel</button>
+    </div>
+  )
+}
+
+export default Logout
