d18f9a2afc45711d87233beabc4c442f21464ef2
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 17:05:23 +0330

    Modified create_till_date command to accept an EmployeeId instead of salon

--- scheduler/management/commands/create_till_date.py
+++ scheduler/management/commands/create_till_date.py
@@ -1,19 +1,19 @@
 from django.core.management.base import BaseCommand
-from scheduler.models import Salon
-
+from scheduler.models import AppointmentSlot
+from users.models import SalonEmployee
 
 class Command(BaseCommand):
     help = "Create appointment slots till the given date"
     
     def add_arguments(self, parser):
         parser.add_argument('end_date', type=str, help="The end date in the format 'YYYY-MM-DD'")
-        parser.add_argument("salon", type=str, help="The Salon name")
+        parser.add_argument("employee_id", type=str, help="The Salon name")
     
     def handle(self, *args, **options):
         end_date = options['end_date']
-        salon = options['salon']
-        salon = Salon.objects.get(name=salon)
+        employee_id = options['employee_id']
+        employee = SalonEmployee.objects.get(id=employee_id)
         
         
-        salon.create_slots_until_date(end_date)
+        AppointmentSlot.create_slots_until_date(end_date, employee)
         self.stdout.write(self.style.SUCCESS("Appointment slots created successfully"))
