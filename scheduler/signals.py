1b5c783edbf8a66e78eb179aeb8f2d883b8bec06
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 15:51:59 +0330

    Modified scheduler's signals, it's now logicless

--- scheduler/signals.py
+++ scheduler/signals.py
@@ -4,18 +4,3 @@ from django.dispatch import receiver
 
 from .models import AppointmentSlot
 
-@receiver(post_save, sender=AppointmentSlot)
-def generate_apps(sender, instance, created, **kwargs):
-    """
-    Signal to generate appointments when an AppointmentSlot is created.
-    """
-    if created:
-        service_duration = 60
-        instance.create_appointments(service_duration)
-        
-        
-        
-@receiver(post_save, sender=AppointmentSlot)
-def update_active_status(sender, instance: AppointmentSlot, **kwargs):
-    instance.check_and_update_active_status()
-
