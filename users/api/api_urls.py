55b3267434adf7a5c7e05ebe6c4e0141da1a0fdc
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 15:49:37 +0330

    Modified User's api logic

--- users/api/api_urls.py
+++ users/api/api_urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView
 )
 
-from .api_views import UserDetailView
-
+from .api_views import UserDetailView, session_login, authenticate_with_session
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('user/detail/', UserDetailView.as_view(), name="user_detail"),
 ]
--- users/api/api_views.py
+++ users/api/api_views.py
@@ -1,7 +1,16 @@
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+
+from django.contrib.sessions.models import Session
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import get_user_model
+
 from .serializers import UserSerializer
 
 
+User = get_user_model()
 
 class UserDetailView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -10,3 +19,4 @@ class UserDetailView(generics.RetrieveAPIView):
     
     def get_object(self):
         return self.request.user
+    
