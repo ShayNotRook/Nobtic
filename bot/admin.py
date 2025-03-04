ed0db86f457bb9046635e80c8640993e20be3149
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 18:47:11 +0330

    Added CodeAdmin model

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import VerificationCode
 
 # Register your models here.
+
+@admin.register(VerificationCode)
+class CodeAdmin(admin.ModelAdmin):
+    list_display = ['phone_number', 'code']
