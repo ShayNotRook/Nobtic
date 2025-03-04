36972cdd82064d51bb6c07f5c901dc2a90daece9
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 18:56:27 +0330

    Added bot APIs

--- bot/urls.py
+++ bot/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 
-from .views import ListAppSlots, ListAppsBySlot
+from .views import (GetSalonView, GetAvailableSlots, update_app,
+                    send_verification_sms, verify_code, create_unpaid_app)
 
 urlpatterns = [
-    path('salon/<int:salon_id>/slots', ListAppSlots.as_view(), name='app_slots'),
-    path('slots/<int:slot_id>/', ListAppsBySlot.as_view(), name='apps_by_slot')
+    # <---Initial endpoints to fetch salon, employee and service data--->
+    path('api/salons/<slug:slug>/', GetSalonView.as_view(), name='salon-view'),
+    path('api/available-slots/', GetAvailableSlots.as_view(), name='available-slots'),
+    
+    # <---Phone num verifications--->
+    path('api/send_verification_code/', send_verification_sms, name='send_verify_code'),
+    path('api/verify_code/', verify_code, name='verify_code'),
+    
+    # <---Appointment controllers--->
+    path('api/app/create/', create_unpaid_app, name='create_app'),
+    path('api/app/update/<int:app_id>', update_app, name='update_appointment'),
 ]
