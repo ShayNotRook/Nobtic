421ba1929073be78a7a6459e06d40c3df7fd0217
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2025-03-04 15:50:06 +0330

    Added EmployeeAdmin (ModelAdmin)

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, SalonEmployee
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'is_superuser']
+
+
+@admin.register(SalonEmployee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ["id", 'salon__name', 'name', 'services', 'card_num']
+    
+    
+    def services(self, obj):
+        return obj.get_services()
